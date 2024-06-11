9e779c9847396a937ee848e9eca3c5c8d9a90823
mayankpcp7 <jariwalmayank123@gmail.com>
2024-06-11 12:48:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ChooseUs from "./components/ChooseUs";
+
+function App() {
+  return (
+    <>
+      <ChooseUs />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "Poppins",
+        humanGeometricSans: "hurme-geometric",
+      },
+    },
+  },
+  plugins: [],
+};
